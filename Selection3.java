e90edfa546b52d6192a56130f22a38c24b9acdb4
FebrianArkaSamudra <febrianarka10@gmail.com>
2023-09-29 15:12:25 +0700

    asli

---
+++ Selection3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Selection3{
+    public static void main(String[] args) {
+        Scanner input05 = new Scanner(System.in);
+        double operand1, operand2, results;
+        char operator;
+        System.out.println("Input operand 1: ");
+        operand1 = input05.nextDouble();
+        System.out.println("Input operand 2: ");
+        operand2 = input05.nextDouble();
+        System.out.println("Input operand (+ - * /): ");
+        operator = input05.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                results = operand1 + operand2;
+                System.out.println(operand1 + " + " + operand2 + " = " + results);
+                break;
+            case '-':
+                results = operand1 - operand2;
+                System.out.println(operand1 + " - " + operand2 + " = " + results);
+                break;
+            case '*':
+                results = operand1 * operand2;
+                System.out.println(operand1 + " * " + operand2 + " = " + results);
+                break;
+            case '/':
+                results = operand1 / operand2;
+                System.out.println(operand1 + " / " + operand2 + " = " + results);
+                break;
+        }
+    }
+}
