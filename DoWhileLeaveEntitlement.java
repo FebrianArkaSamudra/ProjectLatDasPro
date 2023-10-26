62f6ce546bbdf88bdece3c1b99a161ed2ac76521
FebrianArkaSamudra <febrianarka10@gmail.com>
2023-10-26 09:25:48 +0700

    Modified no 2

--- DoWhileLeaveEntitlement.java
+++ DoWhileLeaveEntitlement.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
-public class DoWhileLeaveEntitlement{
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int leaveEntitlement, numLeave;
-        String confirmation;
-        System.out.print("Number of Leave Entitlement = ");
-        leaveEntitlement = scan.nextInt();
-
-        do{
-            System.out.print("Do you want to take leave (y/t) = ");
-            confirmation = scan.next();
-
-            if (confirmation.equalsIgnoreCase("y")) {
-                System.out.print("Leave number = ");
-                numLeave = scan.nextInt();
-
-                if (numLeave <= leaveEntitlement) {
-                    leaveEntitlement -= numLeave;
-                    System.out.println("the remaining leave entiltlement = "+ leaveEntitlement);
-                } else {
-                    System.out.println("The remaining leave entitlement is not sufficient!");
-                    break;
-                }
-            } 
-        
-        } while (leaveEntitlement >  0);
+public class DoWhileLeaveEntitlement {
+public static void main(String[] args) {
+    Scanner inp = new Scanner(System.in);
+    int leaveEntitlement, numLeave = 0;
+    String confirmation;
+    System.out.println("Entitlement leave: ");
+    leaveEntitlement = inp.nextInt();
+    do {
+    System.out.println("Do you want to take a leave? Y/N");
+    confirmation = inp.next();
+    if (confirmation.equalsIgnoreCase("y")) {
+    System.out.println("Leave number: ");
+    numLeave = inp.nextInt();
+    if (numLeave <= leaveEntitlement) {
+    leaveEntitlement -= numLeave;
+    System.out.println("Remaining leave allowance: " 
+   + leaveEntitlement);
+    } else {
+    System.out.println("Your inputted value is too greater than your current leave allowance");
+    } if (numLeave == 0) {
+    System.out.println("No leave allowance left.");
+    break;
     }
+    } else {
+    System.out.println("You are a good citizen. Good job not having a life");
+    leaveEntitlement = 0;
+    }
+    } while (leaveEntitlement > 0);
+   
+}  
 }
