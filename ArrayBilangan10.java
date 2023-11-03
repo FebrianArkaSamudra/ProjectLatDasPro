8d67ed7d90e8f509de62e41cf60347c05246c5ea
FebrianArkaSamudra <febrianarka10@gmail.com>
2023-11-03 10:53:44 +0700

    modified 1

--- ArrayBilangan10.java
+++ ArrayBilangan10.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 public class ArrayBilangan10{
     public static void main(String[] args) {
-        int bil[] = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        Scanner scan = new Scanner (System.in);
+        double [] bil = {5.0,12867,7.5,2000000};
         System.out.println(bil[0]);
         System.out.println(bil[1]);
         System.out.println(bil[2]);
         System.out.println(bil[3]);
+        
     }
 }
--- ArrayRataNilai.java
+++ ArrayRataNilai.java
@@ -6,16 +6,20 @@ public class ArrayRataNilai {
         int[] nilaiMhs = new int [10];
         double total = 0;
         double rata2;
-
+        int banyak = 0;
         for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = scan.nextInt();
+            if (nilaiMhs[i] > 70) {
+                banyak++;
+            }
         }
         for (int i = 0; i < nilaiMhs.length;i++){
             total += nilaiMhs[i];
         }
         rata2 =  total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = "+rata2);
+        System.out.println("Banyak Siswa yang lulus adalah " +banyak);
 
     }
 }
