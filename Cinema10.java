7927eef265105ece7bbc82f0cb63f648a491bc2b
FebrianArkaSamudra <febrianarka10@gmail.com>
2023-11-10 13:07:36 +0700

    modified2

--- Cinema10.java
+++ Cinema10.java
@@ -17,9 +17,9 @@ public class Cinema10 {
         System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
 
-        System.out.println(audience.length);
-        for (String[] rowAudience : audience){
-            System.out.println("Length of row : " + rowAudience.length);
+        System.out.println("Audience in the row 3 : ");
+        for (int i = 0; i < audience[2].length; i++){
+            System.out.println(audience[2][i]);
         }
     }
 }
