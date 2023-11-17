3da38ac69c3cbd015f13ee57f86dc0e436f4611c
FebrianArkaSamudra <febrianarka10@gmail.com>
2023-11-17 14:59:48 +0700

    week11

---
+++ individu1Week11.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class individu1Week11 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = scan.nextInt();
+
+        if (N >= 3) {
+            int i = 1;
+            while (i <= N) {
+                int space = 1;
+                while (space <= N - i) {
+                    System.out.print(" ");
+                    space++;
+                }
+
+                int j = 1;
+                while (j <= i) {
+                    System.out.print(j);
+                    j++;
+                }
+                System.out.println();
+                i++;
+            }
+        } else {
+            System.out.println("Minimal angka yang dimasukkan 3 ");
+        }
+    }
+}
---
+++ individu2Week11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class individu2Week11 {
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+System.out.print("Masukkan nilai N = ");
+int N = scan.nextInt();
+if (N>=5) {
+int i = N;
+while (i>=1) {
+    int j = 1;
+    while (j<=i) {
+        System.out.print("*");
+        j++;
+    }
+    System.out.println();
+    i--;
+}
+}
+else{
+    System.out.println("minimal input 5 ");
+}
+
+    }
+}
