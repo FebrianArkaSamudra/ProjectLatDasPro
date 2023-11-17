07448fd998e0bb14d4053e81de59946b236e8d59
FebrianArkaSamudra <febrianarka10@gmail.com>
2023-11-17 11:18:34 +0700

    modified1

--- Quiz.java
+++ Quiz.java
@@ -15,9 +15,23 @@ public class Quiz {
                 int answer = input.nextInt();
                 input.nextLine();
                 success = (answer == number);
+
+                if (answer == number) {
+                    System.out.println("Anda Benar!");
+                    success = true;
+                }else if (answer < number) {
+                    System.out.println("Nilai anda terlalu kecil,coba lagi");
+                }
+                else {
+                    System.out.println("Nilai anda terlalu besar,coba lagi");
+                }
             } while (!success);
-            System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
+            System.out.print("Apakah Anda ingin mengulang permainan (Y/N)?");
             menu = input.nextLine().charAt(0);
         } while (menu=='y' || menu=='y');
+          if (menu == 'n' || menu == 'N'){
+            System.out.println("Thank you");
+                     System.exit(0);
+          }
     }
 }
