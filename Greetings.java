99e27a275a6ba08167b36159cb91c0de977d9f72
FebrianArkaSamudra <febrianarka10@gmail.com>
2023-11-24 13:08:11 +0700

    week13

--- Greetings.java
+++ Greetings.java
@@ -1,4 +1,15 @@
-public class Greetings {
+import java.util.Scanner;
 
-    
+public class Greetings {
+    public static String getGreetingRecipient(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the name of people you want to greet : ");
+        String recipientName = input.nextLine();
+        input.close();
+        return recipientName;
+    }
+    public static void main(String[] args) {
+        String name = getGreetingRecipient();
+        System.out.println("Thank You " + name+" May the force be with you");
+    }
 }
