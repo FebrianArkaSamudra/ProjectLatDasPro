0f5b15a9e5ecd97d0f0192339cf0fd5d30fa4edf
FebrianArkaSamudra <febrianarka10@gmail.com>
2023-11-24 11:21:36 +0700

    week 13,exp 1

---
+++ Gratitude.java
@@ -0,0 +1,9 @@
+public class Gratitude {
+    public static void sayThankyou(){
+        System.out.println("Thaknyou for being the best teacher in the world.\n"+
+        "you inspired in me a love for learnng and made me feel like i could ask you anything. ");
+    }
+    public static void main(String[] args) {
+        sayThankyou();
+    }
+}
