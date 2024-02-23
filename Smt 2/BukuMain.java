de994091acc454f241236ec29b1bc7f70f773f89
FebrianArkaSamudra <febrianarka10@gmail.com>
2024-02-23 14:35:54 +0700

    commit

---
+++ Smt 2/BukuMain.java
@@ -0,0 +1,18 @@
+public class BukuMain {
+
+    public static void main(String[] args) {
+        Object bk1 = new Object();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+
+        
+    }
+}
