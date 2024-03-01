c04c0808471b4e872189ab23b329e2b72984bec7
FebrianArkaSamudra <febrianarka10@gmail.com>
2024-03-01 13:20:36 +0700

    Latihan Praktikum

--- Smt 2/BukuMain.java
+++ Smt 2/BukuMain.java
@@ -12,13 +12,32 @@ public class BukuMain {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+        bk1.hitungHargaTotal(5);
+        bk1.hitungDiskon(5);
+        bk1.hitungHargaBayar(5);
+
+        System.out.println("\n===================\n");
 
         Object bk2 = new Object("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
         bk2.tampilInformasi();
+        bk2.gantiHarga(45000);
+        bk2.tampilInformasi();
+        bk2.hitungHargaTotal(11);
+        bk2.hitungDiskon(11);
+        bk2.hitungHargaBayar(11);
+
+        System.out.println("\n===================\n"); 
 
         Object bukuarka = new Object("Febrian Arka", "arka", 170, 45, 75000);
         bukuarka.terjual(20);
         bukuarka.tampilInformasi();
+        bukuarka.gantiHarga(70000);
+        bukuarka.tampilInformasi();
+        bukuarka.hitungHargaTotal(20);
+        bukuarka.hitungDiskon(20);
+        bukuarka.hitungHargaBayar(20);
+
+        System.out.println("\n===================\n");
     }
 }
--- Smt 2/Object.java
+++ Smt 2/Object.java
@@ -37,5 +37,22 @@ public class Object {
         harga = hrg;
     }
 
-                            
+    int hitungHargaTotal(int jml){
+        return harga*jml;
+    }
+     
+    int hitungDiskon(int jml){
+        int diskon = 0;
+        int hargaTotal = hitungHargaTotal(jml);
+        if (hargaTotal>=150000) {
+            diskon = (int)(0.12*hargaTotal);
+        }else if (hargaTotal<150000 && hargaTotal>=75000)
+        diskon = (int)(0.05*hargaTotal);
+        else
+        diskon = 0;
+        return diskon;
+    }
+    int hitungHargaBayar(int jml){
+        return hitungHargaTotal(jml) - hitungDiskon(jml);
+    }
 }
