ace11ed92662b655e37033eb9be9468e6ee48cb6
FebrianArkaSamudra <febrianarka10@gmail.com>
2024-03-02 18:03:15 +0700

    percobaan 3

---
+++ Smt 2/Blocks.java
@@ -0,0 +1,25 @@
+public class Blocks {
+    
+    public int width, length, height;
+
+    public Blocks(int p, int l, int t){
+        length = p;
+        width = l;
+        height = t;
+    }
+
+    public int countVolume(){
+        return length*width*height;
+    }
+    public class ArrayBlocks {
+        public static void main(String[] args) {
+            Blocks[] blArray = new Blocks[3];
+            blArray[0] = new Blocks(100, 30, 12);
+            blArray[1] = new Blocks(120, 40,15);
+            blArray[2] = new Blocks(210, 50, 25);
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Volume blocks - " + i + " : " + blArray[i].countVolume());
+            }
+        }
+    }
+}
