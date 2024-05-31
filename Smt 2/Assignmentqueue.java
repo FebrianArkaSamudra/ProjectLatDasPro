df3760d86a256daaaae12ea2a262c511152b793b
FebrianArkaSamudra <febrianarka10@gmail.com>
2024-05-31 20:07:56 +0700

    Assignmentweek10

---
+++ Smt 2/Assignmentqueue.java
@@ -0,0 +1,13 @@
+public class Assignmentqueue {
+    String name,
+    nim;
+    int classNumber;
+    double gpa;
+
+    public Assignmentqueue(String name, String nim, int classNumber, double gpa) {
+        this.name = name;
+        this.nim = nim;
+        this.classNumber = classNumber;
+        this.gpa = gpa;
+    }
+}
