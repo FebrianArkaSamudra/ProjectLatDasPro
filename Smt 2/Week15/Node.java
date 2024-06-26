67d72e6321b0087ef90c0ea5f9df7e83875861ee
FebrianArkaSamudra <febrianarka10@gmail.com>
2024-06-27 00:01:50 +0700

    week15

---
+++ Smt 2/Week15/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    int data;
+    Node next, prev;
+
+    Node(Node prev, int data, Node next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
